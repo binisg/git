1cb310da6f887528fe0c35bd5a5ab61f46d37b39
binisg <binisgiannis@gmail.com>
2021-09-01 10:59:54 +0300

    Changes in buttons and heading

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 let nmbrs = document.getElementById('number'); // get the 'number' element
-nmbrs.textContent = "0";
+nmbrs.textContent = Number(0);
+
+const decrease = document.getElementById('btn1'); // get first button
+const reset = document.getElementById('btn2'); // get second button
+const increase = document.getElementById('btn3'); // get third button
+
+function increasing() {
+    nmbrs.textContent++;
+}
+
+function decreasing() {
+    nmbrs.textContent--;
+}
+
+function reseting() {
+    nmbrs.textContent = 0;
+}
+
+increase.addEventListener("click", increasing, true);
+decrease.addEventListener("click", decreasing, true);
+reset.addEventListener("click", reseting, true);
